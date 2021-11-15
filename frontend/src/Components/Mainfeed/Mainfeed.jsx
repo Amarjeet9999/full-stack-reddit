db8840f2323450ecb17e1c0f89fe3094dfc16ab4
Vipul <Vipulsoniab17@gmail.com>
2021-11-15 21:15:21 +0530

    mainfeed done

---
+++ frontend/src/Components/Mainfeed/Mainfeed.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Mainfeed.css";
+import Sidebar from "../Sidebar/Sidebar";
+
+export default function Mainfeed() {
+  return (
+    <div className="main-feed">
+      {/* <Mainbar /> */}
+      <Sidebar />
+    </div>
+  );
+}
